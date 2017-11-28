d7543ef0953f9dd28fde320d5604ebeed37085cc
Jonathan Havron <jonathan.havron@mcis.com>
2017-11-28 14:53:43 -0600

    move attributes to correct files

--- Wul/StdLib/MagicNetFunctionAttribute.cs
+++ Wul/StdLib/MagicNetFunctionAttribute.cs
@@ -3,17 +3,6 @@
 namespace Wul.StdLib
 {
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
-    internal class NetFunctionAttribute : Attribute
-    {
-        public string Name { get; set; }
-
-        public NetFunctionAttribute(string name)
-        {
-            Name = name;
-        }
-    }
-
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
     internal class MagicNetFunctionAttribute : Attribute
     {
         public string Name { get; set; }
---
+++ Wul/StdLib/NetFunctionAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Wul.StdLib
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    internal class NetFunctionAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public NetFunctionAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
