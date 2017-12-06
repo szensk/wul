f79924199856e44b26cfe38fb9702e16d947bc67
Jonathan Havron <jonathan.havron@mcis.com>
2017-12-06 14:24:05 -0600

    move metamethod to new file

--- Wul/Interpreter/MetaType.cs
+++ Wul/Interpreter/MetaType.cs
@@ -1,54 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using Wul.Interpreter.MetaTypes;
 using Wul.Interpreter.Types;
-using Wul.Parser;
 
 namespace Wul.Interpreter
 {
-    public class MetaMethod 
-    {
-        public IFunction Method
-        {
-            get;
-            set;
-        }
-
-        public bool IsDefined => Method != null;
-
-        public string Name { get; }
-
-        public MetaMethod(string name)
-        {
-            Name = name;
-        }
-
-        public MetaMethod(MetaMethod other)
-        {
-            Method = other.Method;
-            Name = other.Name;
-        }
-
-        public IValue Invoke(List<IValue> arguments, Scope s)
-        {
-            if (Method == null)
-            {
-                IValue lhs = arguments.First();
-                throw new NotSupportedException($"Unable to invoke metamethod `{Name}` on type {lhs.Type.Name}");
-            }
-
-            if (Method.MetaType == FunctionMetaType.Instance)
-            {
-                return Method.Evaluate(arguments, s);
-            }
-            else
-            {
-                return Method.Execute((ListNode) arguments[1], s);
-            }
-        }
-    }
-
     public abstract class MetaType
     {
         protected MetaType()
