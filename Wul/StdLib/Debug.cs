d88df4e105c637ebdd01a44582e9a2d8682d0c7a
Jonathan Havron <jonathan.havron@mcis.com>
2018-02-21 13:46:40 -0600

    add debug gc functions

--- Wul/StdLib/Debug.cs
+++ Wul/StdLib/Debug.cs
@@ -11,5 +11,26 @@ namespace Wul.StdLib
         {
             return new Sentinel();
         }
+
+        [NetFunction("gc.collect")]
+        internal static IValue CollectGarbage(List<IValue> list, Scope s)
+        {
+            System.GC.Collect();
+            return Value.Nil;
+        }
+
+        [NetFunction("gc.usage")]
+        internal static IValue MemoryUsage(List<IValue> list, Scope s)
+        {
+            long workingSet = System.Diagnostics.Process.GetCurrentProcess().WorkingSet64;
+            return new NetObject(workingSet);
+        }
+
+        [NetFunction("gc.usage.mb")]
+        internal static IValue MemoryUsageMegabytes(List<IValue> list, Scope s)
+        {
+            long workingSet = System.Diagnostics.Process.GetCurrentProcess().WorkingSet64 / (1024 * 1024);
+            return (Number) workingSet;
+        }
     }
 }
